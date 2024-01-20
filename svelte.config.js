a50c2acfe40a4d6c3dcdfc70e4fb99c3765cefaa
Hyeok Kim <hyeokkim@Hyeoks-MacBook-Pro-2.local>
2024-01-20 15:47:46 -0600

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: 'https://see-mike-out.github.io/erie-editor'
+			base: `/erie-editor`
 			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
