49a0d9b40df0a20f35e5205406d6d7099eafeb40
Hyeok Kim <hyeokkim@Hyeoks-MacBook-Pro-2.local>
2024-01-25 16:09:16 -0600

    missing

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,8 @@ const config = {
 			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH)
 		},
 		prerender: {
-			handleHttpError: 'ignore'
+			handleHttpError: 'ignore',
+			handleMissingId: 'ignore'
 		}
 	}
 };
