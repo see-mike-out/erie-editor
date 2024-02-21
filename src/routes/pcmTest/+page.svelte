ce5998680033d30825152b69fc8a31ec1d6cddc9
Hyeok Kim <hyeokkim@Hyeoks-MacBook-Pro-2.local>
2024-02-20 22:10:22 -0600

    versioning

--- src/routes/pcmTest/+page.svelte
+++ src/routes/pcmTest/+page.svelte
@@ -2365,10 +2365,9 @@
   };
 
   let buffer;
-
+  
   onMount(() => {
-    generatePCMCode(queue).then((buf) => {
-      console.log(buf.getChannelData(0));
+    generatePCMCode(queue2).then((buf) => {
       buffer = buf;
     });
   });
@@ -2382,12 +2381,66 @@
       source.onended = () => {
         console.log("done");
       };
+
+      const analyser = ctx.createAnalyser();
+      analyser.fftSize = 2048;
+
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+      analyser.getByteTimeDomainData(dataArray);
+
+      // Connect the source to be analysed
+      source.connect(analyser);
+
+      // Get a canvas defined with ID "oscilloscope"
+      const canvas = document.getElementById("oscilloscope");
+      const canvasCtx = canvas.getContext("2d");
+
+      // draw an oscilloscope of the current audio source
+
+      function draw() {
+        requestAnimationFrame(draw);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        canvasCtx.fillStyle = "rgb(200 200 200)";
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = "rgb(0 0 0)";
+
+        canvasCtx.beginPath();
+
+        const sliceWidth = (canvas.width * 1) / bufferLength;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+          const v = dataArray[i] / 128.0;
+          const y = (v * canvas.height) / 2;
+
+          if (i === 0) {
+            canvasCtx.moveTo(x, y);
+          } else {
+            canvasCtx.lineTo(x, y);
+          }
+
+          x += sliceWidth;
+        }
+
+        canvasCtx.lineTo(canvas.width, canvas.height / 2);
+        canvasCtx.stroke();
+      }
+
+      draw();
     }
   }
 </script>
 
-<button
-  on:click={() => {
-    play();
-  }}>Start</button
->
+<div>
+  <button
+    on:click={() => {
+      play();
+    }}>Start</button
+  >
+</div>
+<canvas id="oscilloscope" style="width: 100vw;"></canvas>
